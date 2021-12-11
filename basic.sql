06f5e62bd0db36a990e5e8b1e0e4b36d51cd5f52
_kumar_aankit_ <75765904+Ankit52790@users.noreply.github.com>
2021-12-11 15:54:56 +0530

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -17,3 +17,24 @@ values(1,'ankit','IT','Azamgarh','aankitkumar52790@gmail.com'),
 (5,'div','CE','SUL','div@gmail.com');
 select * from ankit1.student;
 select email from student where email=div;
+
+
+--------------------------------------------------------------------------------
+
+create database employee;
+use employee;
+create table employee_details(
+employee_id int primary key,
+name1 varchar(40),
+dep varchar(40),
+address varchar(40),
+email varchar(40)
+);
+desc employee_details;
+insert into employee(employee_id,name1,dep,address,email)
+values(1,'ankit','IT','Azamgarh','aankitkumar52790@gmail.com'),
+(2,'ashish','IT','bijnor','ashish@gmail.com'),
+(3,'dib','EE','Allahabad','diba@gmail.com'),
+(4,'ans','upsc','Jaunpur','ans@gmail.com'),
+(5,'div','CE','SUL','div@gmail.com');
+select *fromeml
