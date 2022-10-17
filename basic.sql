a3b88970c3f2df42bb58b2096da05272254cc962
_kumar_ankit_ <75765904+Ankit52790@users.noreply.github.com>
2022-10-17 23:11:17 +0530

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -1,40 +1,118 @@
+# Creating a database 
+create database ankit;
+# del the database 
+drop database ankit;
+# create table in Database 
 create database ankit1;
-use ankit1;
 drop database ankit1;
-create table student(
-student_id int primary key,
-name1 varchar(40),
-major varchar(40),
-address varchar(40),
-email varchar(40)
+create database ankit1;
+
+create table stu(
+id int not null auto_increment,
+sname varchar(20),
+address varchar(30),
+primary key(id)
 );
-desc student;
-insert into student(student_id,name1,major,address,email)
-values(1,'ankit','IT','Azamgarh','aankitkumar52790@gmail.com'),
-(2,'ashish','IT','bijnor','ashish@gmail.com'),
-(3,'dib','EE','Allahabad','diba@gmail.com'),
-(4,'ans','upsc','Jaunpur','ans@gmail.com'),
-(5,'div','CE','SUL','div@gmail.com');
-select * from ankit1.student;
-select email from student where email=div;
-
-
---------------------------------------------------------------------------------
-
-create database employee;
-use employee;
-create table employee_details(
-employee_id int primary key,
-name1 varchar(40),
-dep varchar(40),
-address varchar(40),
-email varchar(40)
+CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-desc employee_details;
-insert into employee(employee_id,name1,dep,address,email)
-values(1,'ankit','IT','Azamgarh','aankitkumar52790@gmail.com'),
-(2,'ashish','IT','bijnor','ashish@gmail.com'),
-(3,'dib','EE','Allahabad','diba@gmail.com'),
-(4,'ans','upsc','Jaunpur','ans@gmail.com'),
-(5,'div','CE','SUL','div@gmail.com');
-select *fromeml
+CREATE DATABASE databasename;
+drop Database testDB;
+CREATE DATABASE testDB;
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+SHOW DATABASES; # to see all the existing databases 
+# The BACKUP DATABASE statement is used in SQL Server to create a full back up of an existing SQL database.
+backup database testDB
+TO DISK = 'D:\backups\testDB.bak';
+drop table Persons;
+drop database ankit1;
+create database kumar;
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+drop database testdb1;
+# here we dropped all the databases 
+create database testDB;
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+create database testdb1;
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+drop table persons; # del the table 
+# select stamement 
+select * from persons;
+select LastName,FirstName from persons; # specified columns 
+
+# Where clause is used to filter records  # if we need only certain records from columns 	
+
+select  Lastname 
+from persons 
+where city='AZAMGARH';
+# operator AND 
+select * from persons
+WHERE Lastname ='kumar' AND firstname='ankit';
+# or operator 
+select * from persons
+WHERE Lastname ='kumar' or firstname='ankit';
+# not operator 
+select * from persons
+WHERE not Lastname ='kumar';
+
+# insert into : if we want to insert new records and new data into table then we can use insert queries
+
+insert into Persons (firstname,lastname,address,city)
+values ('ankit','kumar','Pawai','Azamgarh');
+
+select * from persons;
+
+# Aggregate function : this returns the no of rows that match specified criteria 
+
+select count (PersonID)
+from persons;
+select sum (PersonID)
+from persons;
+select avg(PersonID)
+from persons;
+select firstname,lastname, min (PersonID)
+from persons;
+select firstname,lastname, max (PersonID)
+from persons;
+
+
+# Groupby :  in sql to arrange idential data into groups with the help of some function 
+
+select count (personID),city
+from persons
+group by city;
+
+# having : Used to place conditions where we need to decide which group will be part of final result set 
+
+select firstname, sum(marks)
+from persons 
+group by firstname
+having sum(marks)>500;
